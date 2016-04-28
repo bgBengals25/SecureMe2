14bffeff9f8314005426f13a0e72dcb0bb815d0b
Peter Toth <ptoth585@gmail.com>
2016-04-27 20:56:35 -0400

    added module interface

--- backend/module.py
+++ backend/module.py
@@ -5,5 +5,7 @@ class Module(object):
     __metaclass__ = ABCMeta
     @abstractproperty
     def name(self): pass
+    @abstractproperty
+    def out(self): pass
     @abstractmethod
     def run(self): pass
